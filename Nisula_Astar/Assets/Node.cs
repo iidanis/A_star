eb361de5f9c872ca104f535a790a27c9327ed613
ciini004 <ciini004@edu.xamk.fi>
2023-12-20 14:21:33 +0200

    video 3, min 8

--- Nisula_Astar/Assets/Node.cs
+++ Nisula_Astar/Assets/Node.cs
@@ -6,9 +6,20 @@ public class Node
     public bool walkable;
     public Vector3 worldPosition;
 
+    public int gCost;
+    public int hCost;
+
     public Node (bool _walkable, Vector3 _worldPos)
     {
         walkable = _walkable;
         worldPosition = _worldPos;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
     }
 }
