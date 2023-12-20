802786c8639b62e3334eb3dd0d52b82530f4e35d
ciini004 <ciini004@edu.xamk.fi>
2023-12-20 17:28:28 +0200

    final commit
    + video

--- Nisula_Astar/Assets/Grid.cs
+++ Nisula_Astar/Assets/Grid.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Grid : MonoBehaviour
 {
     //public Transform player;
+    public bool onlyDisplayPathGizmos;
     public LayerMask unwalkableMask;
     public Vector2 gridWorldSize;
     public float nodeRadius;
@@ -22,6 +24,11 @@ public class Grid : MonoBehaviour
         CreateGrid();
     }
 
+    public int MaxSize
+    {
+        get { return gridSizeX * gridSizeY; }
+    }
+
     private void CreateGrid()
     {
         grid = new Node[gridSizeX, gridSizeY];
@@ -34,9 +41,35 @@ public class Grid : MonoBehaviour
             {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint);
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
             }
         }
+    }
+
+    public List<Node> GetNeighbours (Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition)
@@ -53,23 +86,50 @@ public class Grid : MonoBehaviour
         return grid[x,y];
     }
 
+    public List<Node> path;
+
     private void OnDrawGizmos()
     {
         // to visualize the grid
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
-        if (grid != null)
+        if (onlyDisplayPathGizmos)
         {
-            //Node playerNode = NodeFromWorldPoint(player.position);
-            foreach (Node node in grid)
+            if (path != null)
             {
-                Gizmos.color = (node.walkable)?Color.white:Color.red;
-                //if (playerNode == node)
-                //{
-                //    Gizmos.color = Color.cyan;
-                //}
-                Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                foreach (Node node in path)
+                {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                }
             }
         }
+
+        else
+        {
+            if (grid != null)
+            {
+                //Node playerNode = NodeFromWorldPoint(player.position);
+                foreach (Node node in grid)
+                {
+                    Gizmos.color = (node.walkable)?Color.white:Color.red;
+                    //if (playerNode == node)
+                    //{
+                    //    Gizmos.color = Color.cyan;
+                    //}
+
+                    if (path != null)
+                    {
+                        if (path.Contains(node))
+                        {
+                            Gizmos.color = Color.black;
+                        }
+                    }
+
+                    Gizmos.DrawCube(node.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                }
+            }
+        }
+
     }
 }
--- Nisula_Astar/Assets/Pathfinding.cs
+++ Nisula_Astar/Assets/Pathfinding.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using System.Diagnostics;
 
 public class Pathfinding : MonoBehaviour
 {
+    public Transform seeker, target;
+
     public Grid grid;
 
     private void Awake()
@@ -11,37 +15,116 @@ public class Pathfinding : MonoBehaviour
         grid = GetComponent<Grid>();
     }
 
+    private void Update()
+    {
+        // only find path when space is pressed
+        if (Input.GetButtonDown("Jump"))
+        {
+            FindPath(seeker.position, target.position);
+        }
+    }
+
     void FindPath(Vector3 startPos, Vector3 targetPos)
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
         Node startNode = grid.NodeFromWorldPoint(startPos);
         Node targetNode = grid.NodeFromWorldPoint(targetPos);
 
-        List<Node> openSet = new List<Node>();
+        // HEAP OPTIMIZATION!!
+        Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+        // HEAP OPTIMIZATION ENDS!! 
+
+        // NO HEAP OPTIMIZATION
+        //List<Node> openSet = new List<Node>();
+        // NO HEAP OPTIMIZATION ENDS
+
+
         HashSet<Node> closedSet = new HashSet<Node>();
         openSet.Add(startNode);
 
         while (openSet.Count > 0)
         {
-            Node currentNode = openSet[0];
 
-            for (int i = 1; i < openSet.Count; i++) {
-                
-                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
-                {
-                    currentNode = openSet[i];
-                }
-            }
+            // HEAP OPTIMIZATION!!
+            Node currentNode = openSet.RemoveFirstItemFromHeap();
+            // HEAP OPTIMIZATION ENDS!! 
 
-            openSet.Remove(currentNode);
+            // NO HEAP OPTIMIZATION
+            //Node currentNode = openSet[0];
+
+            //for (int i = 1; i < openSet.Count; i++)
+            //{
+
+            //    if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
+            //    {
+            //        currentNode = openSet[i];
+            //    }
+            //}
+
+            //openSet.Remove(currentNode);
+            // NO HEAP OPTIMIZATION ENDS
+
+
             closedSet.Add(currentNode);
 
             if (currentNode == targetNode)
             {
+                sw.Stop();
+                print("Path found: " + sw.ElapsedMilliseconds + " ms");
+                RetracePath(startNode, targetNode);
                 return;
             }
+
+            foreach (Node neighbour in grid.GetNeighbours(currentNode))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = currentNode;
+
+                    if (!openSet.Contains(neighbour))
+                    {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        
         }
+    }
 
+    void RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
 
-        // min 8 !!!!!!
+        while (currentNode != startNode)
+        { 
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    int GetDistance (Node nodeA, Node nodeB)
+    {
+        int disX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int disY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (disX > disY)
+        {
+            return 14 * disY + 10 * (disX - disY);
+        }
+        return 14 * disX + 10 * (disY - disX);
     }
 }
